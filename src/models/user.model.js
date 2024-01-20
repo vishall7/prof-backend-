b4ef267b0c97a0b1e60303a95754cfc784838e23
vishall7 <vishalbarve2305@gmail.com>
2024-01-20 19:34:21 +0530

    fix stack spelling in ApiError file

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -85,7 +85,7 @@ userSchema.methods.generateRefreshToken = function () {
      {
          expiresIn: process.env.REFRESH_TOKEN_EXPIRY
      }
-    )
+    )  
  }
 
 export const User = mongoose.model("User",userSchema)

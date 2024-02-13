8b3f5ba9074b4cdc47085e0064378b9e28f4b41e
vishall7 <vishalbarve2305@gmail.com>
2024-02-13 19:21:38 +0530

    added login and logout controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,19 @@ import { User } from "../models/user.model.js";
 import { fileUploadTOCloudinary } from "../utils/cloudinary.js";
 import {ApiResponse} from "../utils/APIResponse.js";
 
+    // generating access and refresh token 
+
+    const generateAccessAndRefreshTokens = async (userId)=>{
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave: false});
+
+        return {accessToken,refreshToken}
+    }
+
 const registerUser = asyncHandler(async (req,res)=>{
     //get user data
     // validate the data
@@ -67,6 +80,92 @@ const registerUser = asyncHandler(async (req,res)=>{
     return res.status(200).json(
         new ApiResponse(201,userCreated,"user created successfully")
     )
-
 })
-export {registerUser}
+
+const loginUser = asyncHandler(async (req,res)=>{
+    // hit the login route
+    // enter username,email,password
+    //check if the user or email presents in database 
+    // if yes then proceed to get password if not show the user that he/she is not authorize after entering password
+    // if user presents then dont show user that error and procceds to check password
+    //if password is correct generate access and refresh tokens 
+    //if not redirect to login page and tells that password is incorrect 
+
+    const {username,email,password} = req.body;
+
+    if(!(username && email)){
+        throw new ApiError(400,"username or email required");
+    } 
+
+    const userFounded = await User.findOne({
+        $and: [{username},{email}]
+    })
+
+    if(!userFounded){
+        throw new ApiError(409,"user not found");
+    }
+
+    const isPasswordValid = await userFounded.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(400,"password incorrect")
+    }
+
+    const {accessToken,refreshToken} =  await generateAccessAndRefreshTokens(userFounded._id);
+
+    const loggedInUser = await User.findById(userFounded._id).select(
+        "-password -refreshToken")
+    
+    const options = {
+        httpOnly: true,
+        secure: true 
+    }
+        
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser,accessToken,refreshToken
+            },
+            "user logged in successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async (req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {refreshToken: undefined}
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true 
+    }
+
+    return res.status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(
+        new ApiResponse(200,{},"User Logged out")
+    )
+})
+
+
+
+
+
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -89,3 +89,4 @@ userSchema.methods.generateRefreshToken = function () {
  }
 
 export const User = mongoose.model("User",userSchema)
+
